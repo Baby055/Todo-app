085f431797a97a04d9ae0dbe1bed8c69e5540142
Baby055 <hei.sombiniaina@gmail.com>
2026-07-27 09:50:34 +0300

    feat(ApiExceptionHandler) : added exception

---
+++ src/main/java/com/example/demo/endpoint/rest/controller/ApiExceptionHandler.java
@@ -0,0 +1,16 @@
+package com.example.demo.endpoint.rest.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class ApiExceptionHandler {
+
+    @ExceptionHandler(AccessDeniedException.class)
+    public ResponseEntity<String> handleAccessDenied(AccessDeniedException ex) {
+        return ResponseEntity.status(HttpStatus.FORBIDDEN).body(ex.getMessage());
+    }
+}
