292f724097a37cfa7e52762e4becadd79d6880d1
Baby055 <hei.sombiniaina@gmail.com>
2026-07-27 08:47:00 +0300

    feat(user) : create role and user table

---
+++ src/main/java/com/example/demo/entity/User.java
@@ -0,0 +1,26 @@
+package com.example.demo.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "app_user")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
+}
