ed71712c476e5f574beada65dac2ed0be50fed1f
Baby055 <hei.sombiniaina@gmail.com>
2026-07-27 08:53:22 +0300

    feat(TodoRepository) : updated Repository to New entity(User)

--- src/main/java/com/example/demo/repository/TodoRepository.java
+++ src/main/java/com/example/demo/repository/TodoRepository.java
@@ -1,9 +1,14 @@
 package com.example.demo.repository;
 
 import com.example.demo.entity.Todo;
+import com.example.demo.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
-public interface TodoRepository extends JpaRepository<Todo, Long> {
+public interface TodoRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUsername(String username);
+    boolean existsByUsername(String username);
 }
